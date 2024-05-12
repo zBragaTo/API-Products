cf7ad7975ab229a811e37c5851fca46e25df90ac
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-12 19:03:32 -0300

    Register Product - POST

--- src/main/java/br/com/api/products/controller/ProductController.java
+++ src/main/java/br/com/api/products/controller/ProductController.java
@@ -1,7 +1,10 @@
 package br.com.api.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.products.models.ProductModel;
@@ -12,6 +15,12 @@ public class ProductController {
     
     @Autowired
     private ProductService ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProductModel pm){
+        return ps.cadastrar(pm);
+    }
+
 
     @GetMapping("/listar")
     public Iterable<ProductModel> listar(){
