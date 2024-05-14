9842fb7c6077e206052f0f7648a9e1332bd64053
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-13 23:02:28 -0300

    ADD CORS TO ALL *

--- src/main/java/br/com/api/products/controller/ProductController.java
+++ src/main/java/br/com/api/products/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.com.api.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.api.products.service.ProductService;
 import br.com.api.products.models.AnswerModel;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
     
     @Autowired
