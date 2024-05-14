6f3997e9c02dcfee719c69109dc7d9a0df174da7
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-13 22:42:02 -0300

    Fixing CODE

--- back-end/src/main/java/br/com/api/products/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.products.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.api.products.models.ProductModel;
-
-public interface ProductRepository  extends CrudRepository<ProductModel, Long>{
-    
-}
